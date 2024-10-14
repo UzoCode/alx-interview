a6041753cdf6a7aaeba60edb7e7f9283b5ec880b
UzoCode <akosabenedict@gmail.com>
2024-10-14 16:44:52 +0100

    fixed pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,7 +13,7 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    
+
     triangle = [[1]]  # The first row is always [1]
 
     for i in range(1, n):
@@ -23,5 +23,5 @@ def pascal_triangle(n):
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)  # Last element is always 1
         triangle.append(row)
-    
+
     return triangle
